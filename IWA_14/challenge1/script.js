a7e88258a2f271694451b2f8df411aebec0badd4
ONEZWA22 <126070684+ONEZWA22@users.noreply.github.com>
2023-05-08 00:38:52 +0200

    Add files via upload

---
+++ IWA_14/challenge1/script.js
@@ -0,0 +1,19 @@
+let firstName = 'John';
+let age = 35;
+let hobby = 'Coding';
+
+const logTwice = (logTwice) => {
+
+  console.log(logTwice)
+ 
+  
+}
+
+function john() {
+  logTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`)
+ 
+}
+
+john()
+john()
+
